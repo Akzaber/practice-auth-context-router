5edc358bf02142bdcedf4ddad3cd68696da9576c
Akzaber <zaberkhan611@gmail.com>
2022-10-21 16:07:30 +0600

    context setup complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  }
   return (
     <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col">
@@ -8,7 +17,8 @@ const Login = () => {
       <h1 className="text-5xl font-bold">Please Login now!</h1>
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
+     <form onSubmit={handleSubmit}>
+     <div className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
@@ -21,13 +31,14 @@ const Login = () => {
           </label>
           <input type="password" name='password' placeholder="password" className="input input-bordered" required/>
           <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+            <Link to='/register' className="label-text-alt link link-hover">For the first time here? Register Now</Link>
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button className="btn btn-primary uppercase">Login</button>
         </div>
       </div>
+     </form>
     </div>
   </div>
 </div>
